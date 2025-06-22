56e4e0c3fd54f7fbcd9cf0cd9a80030be4ab17be
Péter Verebes <49650243+Petiazediter@users.noreply.github.com>
2025-06-22 12:09:06 +0200

    feat: type generation for graphql

--- backend/src/api/auth.graphql.ts
+++ backend/src/api/auth.graphql.ts
@@ -1,10 +1,12 @@
+import { Resolvers } from "@generated/graphql";
+
 export const typeDef = `
     extend type Query { 
         ok: Boolean
     }
 `;
 
-export const resolvers = {
+export const resolvers: Resolvers = {
   Query: {
     ok: () => true,
   },
