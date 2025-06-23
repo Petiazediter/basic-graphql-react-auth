7f7af68292c67212caa469c803998b8fa771de9d
Petiazediter <pverebes74@gmail.com>
2025-06-23 22:01:34 +0200

    feat: really basic login with saving token to localStorage

--- frontend/src/auth/AuthForm.tsx
+++ frontend/src/auth/AuthForm.tsx
@@ -1,27 +1,98 @@
-import { useCallback } from 'react';
+import { ApolloError, useMutation, type FetchResult } from '@apollo/client';
+import { useCallback, useState } from 'react';
 import { Controller, FormProvider, useForm, type SubmitHandler } from 'react-hook-form';
+import { SIGN_IN_MUTATION, SIGN_UP_MUTATION } from './AuthForm.graphql';
+import { AuthSignUpDocument, type AuthSignInMutation, type AuthSignInMutationVariables, type AuthSignUpMutation, type AuthSignUpMutationResult, type AuthSignUpMutationVariables } from './__generated__/AuthForm.graphql';
 
 type InputForm = {
   email: string;
   password: string;
+  passwordConfirmation: string;
 }
 
 export const AuthForm = () => {
   const form = useForm<InputForm>({
     defaultValues: {
       email: '',
-      password: ''
+      password: '',
+      passwordConfirmation: ''
     }
   });
 
-  const onSubmit: SubmitHandler<InputForm> = useCallback((formData, event) => {
+  const [isRegister, setIsRegister] = useState(false);
+
+  const [signInMutation, { loading: isSignInMutationLoading }] = useMutation<AuthSignInMutation, AuthSignInMutationVariables>(SIGN_IN_MUTATION)
+  const [signUpMutation, { loading: isSignUpMutationLoading }] = useMutation<AuthSignUpMutation, AuthSignUpMutationVariables>(SIGN_UP_MUTATION)
+
+  const isLoading: boolean = isSignInMutationLoading || isSignUpMutationLoading;
+
+  const onSubmit: SubmitHandler<InputForm> = (formData, event) => {
     event?.preventDefault();
-    console.log('Data!', formData);
-  },[])
+
+    if ( !isRegister) {
+      signInMutation({
+        variables: {
+          input: {
+            email: formData.email,
+            password: formData.password
+          }
+        }
+      }).then((value) => {
+        const valueData = value as FetchResult<AuthSignInMutation>
+        if ( value && value.data ) {
+          const token = valueData.data?.login;
+          if ( token ) {
+            handleAuthSuccess(token);
+          } else {
+            throw new Error('No token found');
+          }
+        }
+      }).catch((reason) => {
+        if ( reason instanceof ApolloError) {
+          console.log({ reason: reason.message })
+        } else {
+          console.error('Unknown error', reason);
+        }
+      })
+    } else {
+      if ( formData.password !== formData.passwordConfirmation ) {
+        throw new ApolloError({ errorMessage: "Passwords do not match" })
+      }
+      signUpMutation({
+        variables: {
+          input: {
+            email: formData.email,
+            password: formData.password
+          }
+        }
+      }).then((value) => {
+        const valueData = value as FetchResult<AuthSignUpMutation>
+        if ( value && value.data ) {
+          const token = valueData.data?.createUser;
+          if ( token ) {
+            handleAuthSuccess(token);
+          } else {
+            throw new Error('No token found');
+          }
+        }
+      }).catch((reason) => {
+        if ( reason instanceof ApolloError) {
+          console.log({ reason: reason.message })
+        } else {
+          console.error('Unknown error', reason);
+        }
+      })
+    }
+  }
+
+  const handleAuthSuccess = useCallback((token: string) => {
+    localStorage.setItem("accessToken", token);
+    console.log("Successfully logged in", token);
+  }, []);
 
   return (
     <FormProvider {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}>
+      <form onSubmit={form.handleSubmit(onSubmit)} style={{ display: 'flex', flexDirection: 'column', gap: '10px', margin: '10px', border: '1px solid #ccc', padding: '15px', borderRadius: '8px'}}>
         <Controller control={form.control} 
           name='email'
           rules={{ required: true }}
@@ -36,7 +107,17 @@ export const AuthForm = () => {
             <input type="password" placeholder='Type your password...' {...field} />
           }
         />
-        <button type='submit'>Submit button</button>
+        { isRegister && (
+          <Controller control={form.control}
+            name="passwordConfirmation"
+            rules={{ required: { value: true, message: "Password confirmation is required" }, minLength: { value: 8, message: "Password must be at least 8 characters long" } }}
+            render={({ field }) => 
+              <input type="password" placeholder='Type your password...' {...field} />
+            }
+          />
+        )}
+        <button type='submit' disabled={isLoading}>{ isRegister ? "Sign up" : "Sign in" }</button>
+        <button type="button" onClick={() => setIsRegister(isRegister => !isRegister)}>I would rather {isRegister ? 'sign in' : 'register'}</button>
       </form>
     </FormProvider>
   )
